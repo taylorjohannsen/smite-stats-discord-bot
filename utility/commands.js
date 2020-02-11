17571c4cdd2ac42cb3be031124b51b56ef26f2f3
Taylor Johannsen <freakshow_tj@yahoo.com>
2020-02-10 21:54:59 -0800

    starting on build discord message

--- utility/commands.js
+++ utility/commands.js
@@ -1,10 +1,29 @@
+const axios = require('axios')
+const { RichEmbed } = require('discord.js')
+
 module.exports = {
-    Ping: {
-        name: '!ping',
-        description: 'Ping!',
-        execute(msg, args) {
-            msg.reply('pong');
-            msg.channel.send('pong');
+    Player: {
+        name: '!player',
+        description: 'Gets player profile data',
+        async execute(msg, args) {
+            let playerData = await axios.get('https://api.smite.taylorjohannsen.com/discord/player/justvincent').then(res => { return res.data })
+
+            const embed = new RichEmbed()
+
+            embed.setTitle(playerData.name)
+            let objArray = Object.entries(playerData)
+
+            for await (let pair of objArray) {
+                embed.message({
+                    name: pair[0],
+                    value: pair[1]
+                })
+            }
+
+            // work on getting this working
+
+
+            msg.channel.send(embed);
         }
     }
 }
